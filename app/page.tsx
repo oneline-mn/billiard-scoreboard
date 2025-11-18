ad5aa2662653d88161c90cb9ede1f4ac3eafc4ce
Khasbilegt Tserenbyambaa <khasbilegt.ts@gmail.com>
2025-11-18 12:12:57 +0800

    useSyncExternalStore to detect hydration

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,42 @@
 "use client";
 
-import { createStore, useStateMachine } from "little-state-machine";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { addPlayer } from "@/components/shared/navbar";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { createStore, useStateMachine } from "little-state-machine";
+import { useSyncExternalStore } from "react";
 
-createStore({
-  players: [],
-});
+createStore({ players: [] });
+
+function useHydration() {
+  return useSyncExternalStore(
+    () => () => { },
+    () => true,
+    () => false
+  );
+}
 
 export default function Home() {
   const { state, actions } = useStateMachine({ actions: { addPlayer } });
+  const isHydrated = useHydration();
 
   return (
     <div className="min-h-screen max-w-3xl mx-auto px-4">
       <div className="flex min-h-screen w-full flex-col pt-20 space-y-4">
         <div className="border rounded-xl">
           <div>
-            {state.players.length === 0 ? (
+            {!isHydrated || state.players.length === 0 ? (
               <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">#</TableHead>
+                    <TableHead>Player Name</TableHead>
+                    <TableHead>Total</TableHead>
+                    <TableHead>Win</TableHead>
+                  </TableRow>
+                </TableHeader>
                 <TableBody>
                   <TableRow>
-                    <TableCell colSpan={3} className="text-center text-muted-foreground">
+                    <TableCell colSpan={4} className="text-center text-muted-foreground">
                       No players yet
                     </TableCell>
                   </TableRow>
